c439723244b9ff900ff62742c5ed56c914a28552
faperezm <felipe_perez@mckinsey.com>
2024-03-27 16:44:06 -0300

    initial commit

---
+++ ms-test/src/main/java/com/home/test/mstest/MsTestApplication.java
@@ -0,0 +1,24 @@
+package com.home.test.mstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsTestApplication.class, args);
+    }
+
+}
+
+@RestController
+class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
